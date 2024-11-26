dc39141c09b319aada310ef8255512079d24c5b7
TataFaria <thayllateixeira@outlook.com>
2024-11-26 16:09:53 -0300

    Test E2E no Fluxo de Cadastro

---
+++ cypress/e2e/createProfile.cy.js
@@ -0,0 +1,14 @@
+describe('Check if create profile page renders the correct components', () => {
+  beforeEach(() => {
+    cy.visit(' http://localhost:5173/cadastro')
+  })
+
+  it('should steps 1 and 2 works', () => {
+    cy.get ('input[type="text"]').type('Testerf Cypress')
+    cy.get ('input[type="email"]').type('thayllateixeira@outook.com')
+    cy.get ('input[type="tel"]').type('992879639')
+    cy.get ('button[type="submit"]').click()
+    cy.get ('input[type="password"]').type('Malu.3099')
+    cy.get('button[type="submit"]').should('be.visible')
+  })
+})
