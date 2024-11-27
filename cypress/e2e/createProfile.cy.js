a5d9db233032b0d5def31534fed9c44897c27d9c
TataFaria <thayllateixeira@outlook.com>
2024-11-27 11:10:06 -0300

    Test homePage

--- cypress/e2e/createProfile.cy.js
+++ cypress/e2e/createProfile.cy.js
@@ -4,7 +4,7 @@ describe('Check if create profile page renders the correct components', () => {
   })
 
   it('should steps 1 and 2 works', () => {
-    cy.get ('input[type="text"]').type('Testerf Cypress')
+    cy.get ('input[type="text"]').type('Tester Cypress')
     cy.get ('input[type="email"]').type('thayllateixeira@outook.com')
     cy.get ('input[type="tel"]').type('992879639')
     cy.get ('button[type="submit"]').click()
